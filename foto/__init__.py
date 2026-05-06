b499b1ae31ef74194861a912d771835a15258d5b
Hurum Maksora Tohfa <ttohfa@brynmawr.edu>
2026-05-05 19:35:58 -0700

    Add files via upload

---
+++ foto/__init__.py
@@ -0,0 +1,11 @@
+
+from .models import MODEL_LABELS, MODEL_REGISTRY, CostTracker, get_model
+
+from .parser import InputParser
+
+from .search import PaperSearcher, PaperTriager
+
+from .pipeline import PDFStore, FigureExtractor, FigureScorer
+
+from .export import build_zip, format_authors, get_confidence, confidence_badge_class
+
